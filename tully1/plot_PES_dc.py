c8401b9e687e20ae25858f0ccebfdc28cb1c0916
Zuxin Jin <Zuxin@mbp.local>
2019-08-02 17:29:42 -0400

    modify plot

--- tully1/plot_PES_dc.py
+++ tully1/plot_PES_dc.py
@@ -7,7 +7,7 @@ dc_imag = np.loadtxt("dc_imag.txt")
 
 plt.plot(eigval[:,0], eigval[:,1])
 plt.plot(eigval[:,0], eigval[:,2])
-plt.plot(dc_real[:,0], dc_real[:,1]/50)
+plt.plot(dc_real[:,0], abs(dc_real[:,1])/50)
 plt.plot(dc_imag[:,0], dc_imag[:,1])
 
 plt.show()
