1a87a3a8efd89971c3ea19dc22347babed852621
Zuxin Jin <Zuxin@mbp.local>
2019-08-01 20:40:52 -0400

    test PES

--- tully1/main.cpp
+++ tully1/main.cpp
@@ -10,9 +10,22 @@ extern const std::complex<double> I(0.0, 1.0);
 int main() {
 	Tully1 model(0.01, 1.6, 0.005, 1.0, 0.3);
 
-	FSSH<Tully1> sys(&model, 0, -0, 0.1, 10);
+	arma::uword nx = 1000;
+	double xmin = -10;
+	double xmax = 10;
+	arma::vec xgrid = arma::linspace(xmin, xmax, nx);
 
-	sys.eigvecs.print();
+	arma::mat x_eigval = arma::zeros(2, nx);
+
+	for (arma::uword i = 0; i < nx; ++i) {
+		x_eigval.col(i) = model.eigval(xgrid(i));
+	}
+
+	x_eigval.save("eigval.txt", arma::raw_ascii);
+
+	//FSSH<Tully1> sys(&model, 0, -0, 0.1, 10);
+
+	//sys.eigvecs.print();
 
 	return 0;
 }
